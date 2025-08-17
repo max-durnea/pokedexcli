4ec27f8f5a3b255e07f4a5c740e857e98f99363f
max-durnea <maxim.durnea@mail.ru>
2025-08-17 13:31:59 +0300

    add basic REPL

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 func main() {
-    fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for ;; {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned_input:=cleanInput(input)
+		fmt.Printf("Your command was: %v\n",cleaned_input[0])
+	}
+
 }
 
 func cleanInput(text string) []string{
 	lowerStr := strings.ToLower(text)
 	parts := strings.Fields(lowerStr)
-
 	return parts
 }
