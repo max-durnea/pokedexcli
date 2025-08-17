3100d19c1ed7ef5d7e88a05a40dfb137e8117434
max-durnea <maxim.durnea@mail.ru>
2025-08-17 13:52:27 +0300

    Add command handling

--- main.go
+++ main.go
@@ -6,14 +6,37 @@ import (
 	"bufio"
 	"os"
 )
+type command struct {
+	name string
+	description string
+	callback func() error
+}
+var Commands map[string]command
 func main() {
+	Commands = map[string]command{
+        "exit": {
+            name:        "exit",
+            description: "Exit the Pokedex",
+            callback:    commandExit,
+        },
+        "help": {
+            name:        "help",
+            description: "Displays a help message",
+            callback:    commandHelp,
+        },
+    }
 	scanner := bufio.NewScanner(os.Stdin)
 	for ;; {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
 		cleaned_input:=cleanInput(input)
-		fmt.Printf("Your command was: %v\n",cleaned_input[0])
+		val,ok := Commands[cleaned_input[0]]
+		if ok {
+			val.callback()
+		}else{
+			fmt.Println("Unknown command")
+		}
 	}
 
 }
@@ -22,4 +45,18 @@ func cleanInput(text string) []string{
 	lowerStr := strings.ToLower(text)
 	parts := strings.Fields(lowerStr)
 	return parts
+}
+
+func commandExit() error {
+	defer os.Exit(0)
+	fmt.Println("Closing the Pokedex... Goodbye!");
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, value := range Commands{
+		fmt.Printf("%v: %v\n",value.name, value.description)
+	}
+	return nil
 }
