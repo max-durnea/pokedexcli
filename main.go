4ba3e0ecfb3f3c1684889d7015b9dd43e66729ba
max-durnea <maxim.durnea@mail.ru>
2025-08-17 15:49:55 +0300

    Add locations listing command

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"strings"
 	"bufio"
 	"os"
+	"github.com/max-durnea/pokedexcli/pokeapi"
 )
 type command struct {
 	name string
 	description string
-	callback func() error
+	callback func(cfg *Config) error
+}
+type Config struct {
+	Next string
+	Prev string
 }
 var Commands map[string]command
 func main() {
@@ -24,7 +29,21 @@ func main() {
             description: "Displays a help message",
             callback:    commandHelp,
         },
+		"map": {
+			name: "map",
+			description: "Lists the next 20 locations everytime it's called",
+			callback: commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Lists the previous 20 locations everytime it's called",
+			callback: commandBmap,
+		},
     }
+	locationConfig := Config{
+		Next: "https://pokeapi.co/api/v2/location-area/",
+		Prev: "",
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for ;; {
 		fmt.Print("Pokedex > ")
@@ -33,7 +52,7 @@ func main() {
 		cleaned_input:=cleanInput(input)
 		val,ok := Commands[cleaned_input[0]]
 		if ok {
-			val.callback()
+			val.callback(&locationConfig)
 		}else{
 			fmt.Println("Unknown command")
 		}
@@ -47,16 +66,47 @@ func cleanInput(text string) []string{
 	return parts
 }
 
-func commandExit() error {
+func commandExit(cfg *Config) error {
 	defer os.Exit(0)
 	fmt.Println("Closing the Pokedex... Goodbye!");
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *Config) error {
 	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
 	for _, value := range Commands{
 		fmt.Printf("%v: %v\n",value.name, value.description)
+	}
+	return nil
+}
+
+func commandMap(cfg *Config) error {
+	locationPage, err := pokeapi.FetchLocationPage(cfg.Next)
+	if(err!=nil){
+		return err
+	}
+	fmt.Println(locationPage.Previous)
+	fmt.Println(locationPage.Next)
+	cfg.Next = locationPage.Next
+	cfg.Prev = locationPage.Previous
+	for _, value := range locationPage.Results {
+		fmt.Println(value.Name)
+	}
+	return nil
+}
+func commandBmap(cfg *Config) error {
+	if(cfg.Next== "https://pokeapi.co/api/v2/location-area/?offset=20&limit=20") {
+		fmt.Println("You are on the first page!\n")
+		return nil
+	}
+	locationPage, err := pokeapi.FetchLocationPage(cfg.Prev)
+	if(err!=nil){
+		return err
+	}
+	cfg.Next = locationPage.Next
+	cfg.Prev = locationPage.Previous
+	for _, value := range locationPage.Results {
+		fmt.Println(value.Name)
 	}
 	return nil
 }
